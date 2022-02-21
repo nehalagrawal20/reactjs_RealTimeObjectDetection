657e56b81bfe88d1380969f960c6b1b6de7e31df
nehalagrawal20 <nehal.200702@gmail.com>
2022-02-21 23:33:42 +0530

    Updates

---
+++ precache-manifest.fceed4b9479891249846044dad625a69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a744b0b0bdc4ea71f054f384537be6ea",
+    "url": "/reactjs_RealTimeObjectDetection/index.html"
+  },
+  {
+    "revision": "1ae9809771d08cacb5ef",
+    "url": "/reactjs_RealTimeObjectDetection/static/css/main.0cc4add3.chunk.css"
+  },
+  {
+    "revision": "f08abaac4ce5f88368d2",
+    "url": "/reactjs_RealTimeObjectDetection/static/js/2.8a7d9c30.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/reactjs_RealTimeObjectDetection/static/js/2.8a7d9c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae9809771d08cacb5ef",
+    "url": "/reactjs_RealTimeObjectDetection/static/js/main.e8aad554.chunk.js"
+  },
+  {
+    "revision": "f5bda9480fc0e27a13bf",
+    "url": "/reactjs_RealTimeObjectDetection/static/js/runtime-main.bd042cf4.js"
+  }
+]);
